6cde39c1d30633c7cc99cb582b3e73a19b6582bc
klawil <klawil@a9.com>
2020-08-29 07:07:07 -0700

    Target the new port

--- app/helpers/thermostat.py
+++ app/helpers/thermostat.py
@@ -23,7 +23,7 @@ def update_rooms():
 
     try:
       r = requests.get(
-        'http://{}:5000/api/temp'.format(room['ip']),
+        'http://{}/api/temp'.format(room['ip']),
         timeout=0.5
       )
 
